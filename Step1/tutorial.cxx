ba070fcbedebf99bac30d1438e35e45be0a2ad3a
xxh422735676 <422735676@qq.com>
2024-02-07 13:04:19 +0800

    [Step1] Finish Step1 Tutorial
    1. add makefile to automatic build and compile project.
    	usage: `make all`,`./bin/Tutorial NUM`

--- Step1/tutorial.cxx
+++ Step1/tutorial.cxx
@@ -1,13 +1,15 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
+// #include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
 // TODO 11: Include TutorialConfig.h
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
+  std::cout<<"Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
   if (argc < 2) {
     // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
